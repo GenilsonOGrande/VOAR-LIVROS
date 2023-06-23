15cd6bece19fdaf76c960a503a5382795fa694e7
Arthur Jacintho de Oliveira Santos <art.jacintho14@gmail.com>
2023-06-23 12:44:53 -0300

    Update addArq.js

--- js/AddArq/addArq.js
+++ js/AddArq/addArq.js
@@ -1,1 +1,40 @@
+function carregarArquivo() {
+  var fileInput = document.getElementById('fileInput');
+  var file = fileInput.files[0];
+
+  if (file) {
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      var fileContainer = document.getElementById('fileContainer');
+      var fileURL = e.target.result;
+
+      // Verifica o tipo de arquivo
+      var fileType = file.type;
+
+      // Exibe o arquivo conforme o tipo
+      if (fileType.startsWith('image/')) {
+        var imageHTML = '<img src="' + fileURL + '" alt="Imagem" />';
+        fileContainer.innerHTML = imageHTML;
+      } else if (fileType.startsWith('audio/')) {
+        var audioHTML = '<audio controls><source src="' + fileURL + '" type="' + fileType + '"></audio>';
+        fileContainer.innerHTML = audioHTML;
+      } else if (fileType.startsWith('video/')) {
+        var videoHTML = '<video controls><source src="' + fileURL + '" type="' + fileType + '"></video>';
+        fileContainer.innerHTML = videoHTML;
+      } 
+      else if (fileType === 'application/pdf') {
+  var pdfHTML = '<embed src="' + fileURL + '" type="application/pdf" width="100%" height="1000px" />';
+  fileContainer.innerHTML = pdfHTML;
+}
+
+else {
+        var fileLinkHTML = '<a href="' + fileURL + '">Download</a>';
+        fileContainer.innerHTML = fileLinkHTML;
+      }
+    };
+
+    reader.readAsDataURL(file);
+  }
+}
 
