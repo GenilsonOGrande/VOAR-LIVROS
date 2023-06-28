22754a035c72cb43ab0bf235be2ee4f7af0b7e65
Arthur Jacintho de Oliveira Santos <art.jacintho14@gmail.com>
2023-06-28 15:46:12 -0300

    Update RecId.js

--- js/RecBanco/RecId.js
+++ js/RecBanco/RecId.js
@@ -1,1 +1,9 @@
+varIdUserRec = JSON.parse(localStorage.getItem('IdUser'));
 
+// Verifica se o array foi recuperado com sucesso
+if (varIdUserRec !== null) {
+  
+  console.log(varIdUserRec);
+} else {
+  console.log('Users não encontrados.');
+}
