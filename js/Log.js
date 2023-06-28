afca340b5137027fa069d19f8a22efcd1e0b744c
Arthur Jacintho de Oliveira Santos <art.jacintho14@gmail.com>
2023-06-28 12:31:44 -0300

    Update Log.js

--- js/Log.js
+++ js/Log.js
@@ -1,57 +1,23 @@
-// Recupera o array (banco dados interno/navegador) do localStorage
-var listaUsersRec = JSON.parse(localStorage.getItem('BancoUsers'));
-
-// Verifica se o array foi recuperado com sucesso
-if (listaUsersRec !== null) {
-  
-  console.log(listaUsersRec);
-} else {
-  console.log('Users não encontrados.');
-}
-// Verifica o tipo de usuário com base no email
-function verificarTipoUsuario(email) {
-  var usuario = listaUsersRec.find(function(user) {
-    return user.email === email;
-  });
-  return usuario ? usuario.tipo : null;
-}
-
-// Exemplo de uso
-function checkUserExistenceLog() {
-  const email_login = document.getElementById("email_login").value;  
-  const senha_login = document.getElementById("senha_login").value;
-  
-  const usuarioEncontrado = listaUsersRec.find(user => user.email === email_login && user.senha === senha_login);
-
-  if (usuarioEncontrado) {
-    window.alert("As credenciais são válidas. Bem-vindo, " + usuarioEncontrado.nome + "!");
-    verificarTipoUsuario(usuarioEncontrado.email);
-
-  } else {
-    window.alert("As credenciais são inválidas. Por favor, verifique seu email e senha.");
-  }
-}
-
-// Verificar tipo de usuário ao fazer login
-function verificarTipoUsuario(emailUsuarioLogado) {
-  var tipoUsuarioLogado = verificarTipoUsuario(emailUsuarioLogado);
-
-  if (tipoUsuarioLogado === "adm") {
-    // Funcionalidades exclusivas do Administrador
-    console.log("Bem-vindo, Administrador!");
-    // ...
-  } else if (tipoUsuarioLogado === "user") {
-    // Funcionalidades exclusivas do Usuário Comum
-    console.log("Bem-vindo, Usuário Comum!");
-    // ...
-  } else {
-    // Usuário não encontrado ou tipo inválido
-    console.log("Usuário não encontrado ou tipo inválido.");
-  }
-}
-
+//-----------------------------------------------------------------
 function LogarUser(){
   checkUserExistenceLog();
+}
+//-----------------------------------------------------------------
+function checkUserExistenceLog(){
+const email_login = document.getElementById("email_login").value;  
+const senha_login = document.getElementById("senha_login").value;
+  
+const usuarioEncontrado = listaUsersRec.find(user => user.email === email_login && user.senha === senha_login);
+
+if (usuarioEncontrado) {
+  window.alert("As credenciais são válidas. Bem-vindo, " + usuarioEncontrado.nome + "!");
+  //window.location.href = "https://voar.aero/";
+  verificarTipoUsuario();
+
+} else {
+  window.alert("As credenciais são inválidas. Por favor, verifique seu email e senha.");
+}
+  
 }
 
 //-----------------------------------------------------------------
