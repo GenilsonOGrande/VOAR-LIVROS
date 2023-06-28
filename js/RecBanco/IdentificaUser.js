571e0e8d567ff1c82329a8e794c0a7410ea22915
Arthur Jacintho de Oliveira Santos <art.jacintho14@gmail.com>
2023-06-28 11:19:20 -0300

    Update and rename IdentificaUser to IdentificaUser.js

---
+++ js/RecBanco/IdentificaUser.js
@@ -0,0 +1,25 @@
+//----------------------------------------------
+function verificarTipoUsuario(idUsuario) {
+  var usuario = listaUsersRec.find(function(user) {
+    return user.id === idUsuario;
+  });
+  return usuario ? usuario.tipo : null;
+}
+
+// Exemplo de uso
+var idUsuarioLogado = 1; // ID do usuário logado (exemplo: 1 para Administrador)
+var tipoUsuarioLogado = verificarTipoUsuario(idUsuarioLogado);
+
+if (tipoUsuarioLogado === "admin") {
+  // Funcionalidades exclusivas do Administrador
+  console.log("Bem-vindo, Administrador!");
+  // ...
+} else if (tipoUsuarioLogado === "user") {
+  // Funcionalidades exclusivas do Usuário Comum
+  console.log("Bem-vindo, Usuário Comum!");
+  // ...
+} else {
+  // Usuário não encontrado ou tipo inválido
+  console.log("Usuário não encontrado ou tipo inválido.");
+}
+
