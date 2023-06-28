9d5a1dab85f0d218ec840eb6a8d881808bded901
Arthur Jacintho de Oliveira Santos <art.jacintho14@gmail.com>
2023-06-28 11:57:39 -0300

    Update Log.js

--- js/Log.js
+++ js/Log.js
@@ -11,7 +11,8 @@ const usuarioEncontrado = listaUsersRec.find(user => user.email === email_login 
 
 if (usuarioEncontrado) {
   window.alert("As credenciais são válidas. Bem-vindo, " + usuarioEncontrado.nome + "!");
-  window.location.href = "https://voar.aero/";
+  //window.location.href = "https://voar.aero/";
+  verificarTipoUsuario(idUsuario);
 
 } else {
   window.alert("As credenciais são inválidas. Por favor, verifique seu email e senha.");
