a6e5fde65b0240a846718502f1183209aaf432a9
Arthur Jacintho de Oliveira Santos <art.jacintho14@gmail.com>
2023-06-28 11:06:23 -0300

    Update RecUsers.js

--- js/RecBanco/RecUsers.js
+++ js/RecBanco/RecUsers.js
@@ -8,6 +8,31 @@ if (listaUsersRec !== null) {
 } else {
   console.log('Users não encontrados.');
 }
+//----------------------------------------------
+function verificarTipoUsuario(idUsuario) {
+  var usuario = listaUsersRec.find(function(user) {
+    return user.id === idUsuario;
+  });
+  return usuario ? usuario.tipo : null;
+}
+
+// Exemplo de uso
+var idUsuarioLogado = 1; // ID do usuário logado (exemplo: 1 para Administrador)
+var tipoUsuarioLogado = verificarTipoUsuario(idUsuarioLogado);
+
+if (tipoUsuarioLogado === "admin") {
+  // Funcionalidades exclusivas do Administrador
+  console.log("Bem-vindo, Administrador!");
+  // ...
+} else if (tipoUsuarioLogado === "user") {
+  // Funcionalidades exclusivas do Usuário Comum
+  console.log("Bem-vindo, Usuário Comum!");
+  // ...
+} else {
+  // Usuário não encontrado ou tipo inválido
+  console.log("Usuário não encontrado ou tipo inválido.");
+}
+
 /*
 # Autor: Arthur J de Oliveira Santos
 # Data: 21 de junho de 2023
