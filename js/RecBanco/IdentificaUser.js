8d234ac8d80d168bccd355a375e0aa769ca8e0a0
Arthur Jacintho de Oliveira Santos <art.jacintho14@gmail.com>
2023-06-28 12:17:02 -0300

    Update IdentificaUser.js

--- js/RecBanco/IdentificaUser.js
+++ js/RecBanco/IdentificaUser.js
@@ -1,25 +1,16 @@
-//----------------------------------------------
-function verificarTipoUsuario(idUsuario) {
-  var usuario = listaUsersRec.find(function(user) {
-    return user.id === idUsuario;
-  });
-  return usuario ? usuario.tipo : null;
-}
+/*function verificarTipoUsuario(emailUsuarioLogado) {
+  var tipoUsuarioLogado = verificarTipoUsuario(emailUsuarioLogado);
 
-// Exemplo de uso
-var idUsuarioLogado = 1; // ID do usuário logado (exemplo: 1 para Administrador)
-var tipoUsuarioLogado = verificarTipoUsuario(idUsuarioLogado);
-
-if (tipoUsuarioLogado === "adm") {
-  // Funcionalidades exclusivas do Administrador
-  console.log("Bem-vindo, Administrador!");
-  // ...
-} else if (tipoUsuarioLogado === "user") {
-  // Funcionalidades exclusivas do Usuário Comum
-  console.log("Bem-vindo, Usuário Comum!");
-  // ...
-} else {
-  // Usuário não encontrado ou tipo inválido
-  console.log("Usuário não encontrado ou tipo inválido.");
-}
-
+  if (tipoUsuarioLogado === "adm") {
+    // Funcionalidades exclusivas do Administrador
+    console.log("Bem-vindo, Administrador!");
+    // ...
+  } else if (tipoUsuarioLogado === "user") {
+    // Funcionalidades exclusivas do Usuário Comum
+    console.log("Bem-vindo, Usuário Comum!");
+    // ...
+  } else {
+    // Usuário não encontrado ou tipo inválido
+    console.log("Usuário não encontrado ou tipo inválido.");
+  }
+}*/
