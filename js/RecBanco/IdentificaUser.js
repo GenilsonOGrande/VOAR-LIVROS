3914632c94602467ae21878703715cc11fb943a6
Arthur Jacintho de Oliveira Santos <art.jacintho14@gmail.com>
2023-06-28 15:15:03 -0300

    Update IdentificaUser.js

--- js/RecBanco/IdentificaUser.js
+++ js/RecBanco/IdentificaUser.js
@@ -6,9 +6,11 @@ function verificarTipoUsuario(idUsuario) {
   if (usuario) {
     if (usuario.email === "adm@gmail.com") {
       console.log("Usuário logado é um Administrador.");
+      console.log("ID: " + idUsuario);
       // Adicionar funcionalidades exclusivas do Administrador
     }  else {
       console.log("Usuário logado é um Usuário Comum.");
+      console.log("ID: " + idUsuario);
     }
   } 
 }
