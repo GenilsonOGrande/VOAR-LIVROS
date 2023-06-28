41bc807fd26e46867c86fce9b623988882737ad6
Arthur Jacintho de Oliveira Santos <art.jacintho14@gmail.com>
2023-06-28 12:18:45 -0300

    Update Log.js

--- js/Log.js
+++ js/Log.js
@@ -1,24 +1,45 @@
-//-----------------------------------------------------------------
-function LogarUser(){
-  checkUserExistenceLog();
+// Verifica o tipo de usuário com base no email
+function verificarTipoUsuario(email) {
+  var usuario = listaUsersRec.find(function(user) {
+    return user.email === email;
+  });
+  return usuario ? usuario.tipo : null;
 }
-//-----------------------------------------------------------------
-function checkUserExistenceLog(){
-const email_login = document.getElementById("email_login").value;  
-const senha_login = document.getElementById("senha_login").value;
-  
-const usuarioEncontrado = listaUsersRec.find(user => user.email === email_login && user.senha === senha_login);
 
-if (usuarioEncontrado) {
-  window.alert("As credenciais são válidas. Bem-vindo, " + usuarioEncontrado.nome + "!");
-  //window.location.href = "https://voar.aero/";
-  verificarTipoUsuario();
-
-} else {
-  window.alert("As credenciais são inválidas. Por favor, verifique seu email e senha.");
-}
+// Exemplo de uso
+function checkUserExistenceLog() {
+  const email_login = document.getElementById("email_login").value;  
+  const senha_login = document.getElementById("senha_login").value;
   
+  const usuarioEncontrado = listaUsersRec.find(user => user.email === email_login && user.senha === senha_login);
+
+  if (usuarioEncontrado) {
+    window.alert("As credenciais são válidas. Bem-vindo, " + usuarioEncontrado.nome + "!");
+    verificarTipoUsuario(usuarioEncontrado.email);
+
+  } else {
+    window.alert("As credenciais são inválidas. Por favor, verifique seu email e senha.");
+  }
 }
+
+// Verificar tipo de usuário ao fazer login
+function verificarTipoUsuario(emailUsuarioLogado) {
+  var tipoUsuarioLogado = verificarTipoUsuario(emailUsuarioLogado);
+
+  if (tipoUsuarioLogado === "adm") {
+    // Funcionalidades exclusivas do Administrador
+    console.log("Bem-vindo, Administrador!");
+    // ...
+  } else if (tipoUsuarioLogado === "user") {
+    // Funcionalidades exclusivas do Usuário Comum
+    console.log("Bem-vindo, Usuário Comum!");
+    // ...
+  } else {
+    // Usuário não encontrado ou tipo inválido
+    console.log("Usuário não encontrado ou tipo inválido.");
+  }
+}
+
 //-----------------------------------------------------------------
 /*
 # Autor: Arthur J de Oliveira Santos
