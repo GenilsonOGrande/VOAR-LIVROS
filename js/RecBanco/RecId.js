9531ae66660063acb4bffb1a9521ae8e69c65166
Arthur Jacintho de Oliveira Santos <art.jacintho14@gmail.com>
2023-06-29 12:47:00 -0300

    Update RecId.js

--- js/RecBanco/RecId.js
+++ js/RecBanco/RecId.js
@@ -7,3 +7,10 @@ if (IdUserRec !== null) {
 } else {
   console.log("Não Funcionou. Id: " + IdUserRec);
 }
+/*
+# Autor: Arthur J de Oliveira Santos
+# Data: 21 de junho de 2023
+# Descrição: Este código possui funcionalidade de recuperar o id do usuario acessado, mante-lo logado.
+# Linkedin: https://www.linkedin.com/in/arthur-jacintho-de-oliveira-santos-3737b8261
+# Github: https://github.com/ArthurJos04
+*/
